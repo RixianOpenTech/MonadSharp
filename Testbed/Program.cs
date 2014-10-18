d4e73ca87d77208e12479b00aefa77084726bc4b
McAravey <mcaravey@live.com>
2014-10-18 11:58:30 -0700

    Fixed up if's

--- Testbed/Program.cs
+++ Testbed/Program.cs
@@ -1,37 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reactive.Linq;
+using System.Windows.Markup;
 using MsSystem;
 
 namespace Testbed
 {
+    public class GenericType
+    {
+        public GenericType(Type type, object value)
+        {
+            this.Type = type;
+            this.Value = value;
+        }
+
+        public Type Type { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class Context<T> : Dictionary<string, GenericType>
+    {
+        public T Value { get; set; }
+
+        public Context(T value)
+        {
+            this.Value = value;
+        }
+
+        public Context(T value, params Tuple<string, GenericType>[] contextValues)
+            : this(value)
+        {
+            foreach (var contextValue in contextValues)
+            {
+                this.Add(contextValue.Item1, contextValue.Item2);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            //var x = Observable.Return(2);
-            //var y = Observable.Return(5);
+            Sample.Work().Subscribe();
+            //var x = Observable.Return(new Context<int>(2));
+            //var y = Observable.Return(new Context<int>(5, new[] {Tuple.Create("x", new GenericType(x.GetType(), x))}));
+            
             //var z = x * y;
             //Console.WriteLine(z);
             //Console.ReadLine();
 
             //TestValues();
-            Car car = new Car();
-            car.Speed.Do(s => Console.WriteLine("Speed: {0}", s)).Subscribe();
-            var r = from c1 in car.Accelerate()
-                    from c2 in car.Accelerate()
-                    from c3 in car.Accelerate()
-                    from c4 in car.Accelerate()
-                    from c5 in car.Accelerate()
-                    from c6 in car.Accelerate()
-                    select c6;
-            r.Subscribe();
+            //Car car = new Car();
+            //car.Speed.Do(s => Console.WriteLine("Speed: {0}", s)).Subscribe();
+            //var r = from c1 in car.Accelerate()
+            //        from c2 in car.Accelerate()
+            //        from c3 in car.Accelerate()
+            //        from c4 in car.Accelerate()
+            //        from c5 in car.Accelerate()
+            //        from c6 in car.Accelerate()
+            //        select c6;
+            //r.Subscribe();
 
-            _Console.WriteLine(Euler.Problem1()).Subscribe();
-            var p2 = Euler.Problem2().Select(result => result.ToString());
-            _Console.WriteLine(p2).Subscribe();
-            var p3 = Euler.Problem3().Select(result => result.ToString());
-            _Console.WriteLine(p3).Subscribe();
-
+            //_Console.WriteLine(Euler.Problem1()).Subscribe();
+            //var p2 = Euler.Problem2().Select(result => result.ToString());
+            //_Console.WriteLine(p2).Subscribe();
+            //var p3 = Euler.Problem3().Select(result => result.ToString());
+            //_Console.WriteLine(p3).Subscribe();
+            
             Console.ReadLine();
         }
     }
---
+++ Testbed/Sample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using MS.Core;
+using MsSystem;
+using MsSystem.Extensions;
+
+namespace Testbed
+{
+    public class Sample
+    {
+        public static IObservable<Unit> Work()
+        {
+            IObservable<int> x = Observable.Range(7, 10);
+            IObservable<int> y = Observable.Range(13, 10);
+            IObservable<int> ifBlock = x.Add(y);
+            IObservable<int> elseBlock = y.Subtract(x);
+            var result = x.If(xVal => xVal < 10, ifBlock, elseBlock);
+            return _Console.WriteLine(result);
+        }
+    }
+
+    public static class Ex
+    {
+        public static IObservable<TResult> If<TSource, TResult>(
+            this IObservable<TSource> source,
+            Func<TSource, bool> conditional,
+            IObservable<TResult> ifBranch,
+            IObservable<TResult> elseBranch)
+        {
+            return source.Select(value =>
+                                 {
+                                     var result = conditional(value);
+                                     return Observable.If(() => result, ifBranch, elseBranch);
+                                 }).Flatten();
+        }
+    }
+}
