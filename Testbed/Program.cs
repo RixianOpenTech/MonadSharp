88120e4f304e24f19c34fc2c1011308e52be2e10
McAravey <mcaravey@live.com>
2014-11-06 00:21:13 -0800

    Finalizing sample post-compile code.

--- Testbed/Program.cs
+++ Testbed/Program.cs
@@ -6,6 +6,7 @@ using System.Reactive.Linq;
 using System.Windows.Markup;
 using MS.Core;
 using MsSystem;
+using Newtonsoft.Json;
 
 namespace Testbed
 {
@@ -16,7 +17,7 @@ namespace Testbed
             var x = Observable.Return(10);
             var y = Observable.Return(15);
             var point = new Point();
-            _Console.WriteLine(point.ToObservableString()).ContinueWith(
+            var program = _Console.WriteLine(point.ToObservableString()).ContinueWith(
             point.Translate(x, y).ContinueWith(
             _Console.WriteLine(point.ToObservableString()).ContinueWith(
             point.Translate(x, y).ContinueWith(
@@ -27,7 +28,22 @@ namespace Testbed
             _Console.WriteLine(point.ToObservableString()).ContinueWith(
             point.Translate(x, y).ContinueWith(
             _Console.WriteLine(point.ToObservableString())))))))))));
-            //.Subscribe();
+            
+            Debug.WriteLine(JsonConvert.SerializeObject(program, Formatting.Indented));
+            
+            program.Subscribe();
+
+            //Point point = new Point();
+            //Console.WriteLine(point.ToString());
+            //point.Translate(10, 15);
+            //Console.WriteLine(point.ToString());
+            //point.Translate(10, 15);
+            //Console.WriteLine(point.ToString());
+            //point.Translate(10, 15);
+            //Console.WriteLine(point.ToString());
+            //point.Translate(10, 15);
+            //Console.WriteLine(point.ToString());
+            //point.Translate(10, 15);
 
 
             var car = new Car();
@@ -37,7 +53,7 @@ namespace Testbed
             _Console.WriteLine(car.GetSpeed()))));
             //.Subscribe();
 
-            Sample.Work().Subscribe();
+            //Sample.Work().Subscribe();
             
             Console.ReadLine();
         }
